b7fb8a5e4bb3ef7d93034ced6798cc01fbcd130e
Alfonso-Villarreal97 <dtrejov97@gmail.com>
2024-04-21 17:38:42 -0600

    Mejoras en la interfaz index y adición de animacion en quienes somos

---
+++ javascript/animacionSelectNav.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const currentLocation = window.location.href; // Obtiene la URL actual
+    
+    const linksInicio = document.querySelectorAll('.nav-enlace'); // Selecciona todos los enlaces de navegación
+    const linksResto = document.querySelectorAll('.nav-enlace--lightside'); // Selecciona todos los enlaces de navegación
+    
+    linksInicio.forEach(link => {
+        // Compara la URL actual con la URL del enlace
+        if (link.href === currentLocation) {
+            link.classList.add('nav-enlace--selected'); // Agrega la clase 'nav-enlace--activo' al enlace actual
+        } else {
+            link.classList.remove('nav-enlace--selected'); // Elimina la clase 'nav-enlace--activo' de los demás enlaces
+        }
+    });
+
+    linksResto.forEach(link => {
+        // Compara la URL actual con la URL del enlace
+        if (link.href === currentLocation) {
+            link.classList.add('nav-enlace--selectedLight'); // Agrega la clase 'nav-enlace--activo' al enlace actual
+        } else {
+            link.classList.remove('nav-enlace--selectedLight'); // Elimina la clase 'nav-enlace--activo' de los demás enlaces
+        }
+    });
+});
---
+++ javascript/carousel_About.js
@@ -0,0 +1,81 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const items = document.querySelectorAll('.slider .item');
+    let currentItemIndex = 0;
+    let intervalId;
+
+    const showItem = (index) => {
+        items.forEach(item => {
+            item.classList.remove('active', 'blur');
+            item.style.zIndex = "";
+        });
+
+        items[index].classList.add('active');
+        items[index].style.zIndex = "1";
+
+        if (index === 0) {
+            items[items.length - 1].classList.add('blur');
+        } else {
+            items[index - 1].classList.add('blur');
+        }
+    };
+
+    const navigate = (direction) => {
+        if (direction === 'up') {
+            currentItemIndex = (currentItemIndex - 1 + items.length) % items.length;
+        } else if (direction === 'down') {
+            currentItemIndex = (currentItemIndex + 1) % items.length;
+        }
+        showItem(currentItemIndex);
+    };
+
+    const startAutoSlide = () => {
+        intervalId = setInterval(() => {
+            navigate('down');
+        }, 3000); // Intervalo de 3 segundos
+    };
+
+    const stopAutoSlide = () => {
+        clearInterval(intervalId);
+    };
+
+    const iconUp = document.querySelector('.icon-up');
+    const iconDown = document.querySelector('.icon-down');
+
+    iconUp.addEventListener('click', () => {
+        // console.log('Clic en icono hacia arriba');
+        navigate('up');
+        stopAutoSlide(); // Detener el avance automático al hacer clic manualmente        
+    });
+
+    iconUp.addEventListener('mouseleave', () => {
+        startAutoSlide();
+    });
+    
+    iconDown.addEventListener('click', () => {
+        // console.log('Clic en icono hacia abajo');
+        navigate('down');
+        stopAutoSlide(); // Detener el avance automático al hacer clic manualmente        
+    });
+
+    iconDown.addEventListener('mouseleave', () => {
+        startAutoSlide();
+    });
+
+    // Iniciar el avance automático al cargar la página
+    startAutoSlide();
+
+    // Detener el avance automático al pasar el ratón sobre el slider
+    document.querySelector('.slider').addEventListener('mouseenter', () => {
+        stopAutoSlide();
+    });
+
+   
+
+    // Reanudar el avance automático al quitar el ratón del slider
+    document.querySelector('.slider').addEventListener('mouseleave', () => {
+        startAutoSlide();
+    });
+
+    // Mostrar el primer ítem al cargar la página
+    showItem(0);
+});
